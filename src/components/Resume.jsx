56a5716d6c2927a9cde036c46affeff4b3bcbda5
AKKHILESHRP <akkhileshrp2003@gmail.com>
2023-11-24 13:37:14 +0530

    Resume has been fixed

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./Resume.module.css";
+import akkhileshResumePDF from "./components/data/akkhilesh_resume.pdf";
 
 const Resume = () => {
   return (
-    <a className={styles.downloadBtn} href="https://www.overleaf.com/project/64edb34797e0dc95127ae8be" download>
+    <a className={styles.downloadBtn} href={akkhileshResumePDF} download>
       Download CV
     </a>
   );
