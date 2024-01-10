85a07c47d50f55d6640c59711a07fc4bfe8d7c12
AKKHILESHRP <akkhileshrp2003@gmail.com>
2024-01-10 16:06:14 +0530

    Resume Updated

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Resume.module.css";
-import akkhileshResumePDF from "./data/akkhilesh_resume_updated.pdf";
+import akkhileshResumePDF from "./data/Akkhilesh_R_P.pdf";
 
 const Resume = () => {
   return (
